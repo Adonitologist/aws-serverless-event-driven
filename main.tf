9de58064fe833fad040cbbd56e366ad44d59adc9
Adonitologist <ID+Adonitologist@users.noreply.github.com>
2026-09-21 02:00:03 +0200

    feat: complete parameterization and validate enterprise event-driven workflow

--- main.tf
+++ main.tf
@@ -12,27 +12,6 @@ module "api_eventbridge" {
   source        = "./modules/api_eventbridge"
   environment   = var.environment
   sns_topic_arn = module.messaging.sns_topic_arn
-}
-
-module "compute_storage" {
-  source              = "./modules/compute_storage"
-  environment         = var.environment
-  sqs_queue_arn       = module.messaging.sqs_main_queue_arn
-  kms_key_arn         = module.messaging.kms_key_arn
-  replica_kms_key_arn = module.messaging.kms_replica_key_arn
-  secondary_region    = var.secondary_region
-}
-
-module "networking_dr" {
-  source       = "./modules/networking_dr"
-  environment  = var.environment
-  api_endpoint = module.api_eventbridge.api_endpoint
-}
-
-module "api_eventbridge" {
-  source        = "./modules/api_eventbridge"
-  environment   = var.environment
-  sns_topic_arn = module.messaging.sns_topic_arn
   api_route_key = var.api_route_key
 }
 
@@ -47,4 +26,10 @@ module "compute_storage" {
   lambda_memory_size    = var.lambda_memory_size
   lambda_timeout        = var.lambda_timeout
   dynamodb_billing_mode = var.dynamodb_billing_mode
+}
+
+module "networking_dr" {
+  source       = "./modules/networking_dr"
+  environment  = var.environment
+  api_endpoint = module.api_eventbridge.api_endpoint
 }
